eb3d9ad590b258dffefbc9fa1dd4c0dde9e47d89
kristenomahony <95743303+kristenomahony@users.noreply.github.com>
2022-10-06 09:50:42 -0700

    removed event handler code

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 const hamburgerBtn = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 
-const thanksPopUp = document.getElementById('thanks')
-const sendEmailBtn = document.getElementById('send-email')
+
 
 function toggleButton() {
     navList.classList.toggle('show')
 }
 
-function showThankYou() {
-    thanksPopUp.classList.toggle('show-pop-up')
-}
+
 
 hamburgerBtn.addEventListener('click', toggleButton)
-sendEmailBtn.addEventListener('click', showThankYou)
+
